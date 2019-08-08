2c66eb6176d1096cd02f145103cddf1581bf6516
Alice Reuter <pwn@alicereuter.com>
2019-08-08 13:36:59 -0500

    serving static content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -9,9 +10,15 @@ fn main() {
         handle_connection(stream);
     }
 
-    fn handle_connection(mut stream: TcpStream) {
-        let mut buffer = [0; 512];
-        stream.read(&mut buffer).unwrap();
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    }
+   fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 }
