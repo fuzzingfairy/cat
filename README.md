9fb8f3df9706597c4899d6c1b21d9a3ebd6f781c
Alice Reuter <pwn@alicereuter.com>
2019-08-18 10:29:32 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cat
-This is a rewrite of Villefort in Rust
+This a wep app that stores youtube links kind of like a playlist in rust. This is mainly mean't for me to better understand how to write apis in rust.
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,64 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::error::Error;
 use std::fs;
+use rocket::response::content::Html;
+use rocket::response::NamedFile;
+use rocket_contrib::json::Json;
+use sqlite::State;
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize)]
+struct Video {
+    link: String,
+    category: String
+}
 
+#[get("/")]
+fn index() -> Option<NamedFile> {
+    return NamedFile::open("main.html").ok();
+}
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    get_videos();    
+    return format!("Hello, {}!", name);
+}
+
+#[get("/api/get/videos")]
+fn getVids() -> Json<Video> {
+    Json(Video {
+        link: "https://alicereuter.com".to_string(),
+        category: "blog".to_string()
+    })
+}
+//json
 fn main() {
-    connect_todo();
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
+   // connect_todo();
+    rocket::ignite().mount("/", routes![index,hello,getVids]).launch();
+}
+
+fn get_videos() {
+    let connection = sqlite::open("todo.db").unwrap();
+    let mut statement = connection.prepare("SELECT * FROM vids").unwrap();
+    statement.bind(1,50).unwrap();
+    while let State::Row = statement.next().unwrap() {
+        println!("name = {}", statement.read::<String>(0).unwrap());
     }
-
+    
 }
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    let contents = fs::read_to_string("hello.html").unwrap();
-
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
 
 fn connect_todo () {
     let connection = sqlite::open("todo.db").unwrap();
     connection
         .execute(
             "
-             CREATE TABLE todo (id INTEGER, Description TEXT, Title TEXT, Entered TEXT, Due TEXT, state INTEGER, time INTEGER, Subject TEXT);
+             CREATE TABLE vids (link TEXT, subject TEXT);
              ",
         ).unwrap();
-
 }
+
